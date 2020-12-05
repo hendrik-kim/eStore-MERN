ea6f8f9d90fd431438005ec672af8f9db47c2eb4
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-05 16:38:42 -0600

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <main>
+          <h1>Welcome to Hendrik's shop</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
